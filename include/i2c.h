c4a40b93c61b165f8511a70a93656282af4fad98
Ajaykumar Kannan <kannan.ajay@gmail.com>
2012-07-08 14:12:36 +0530

    Improved comments on header file

--- include/i2c.h
+++ include/i2c.h
@@ -14,16 +14,21 @@ public:
 	~myI2C();
 	
 	// Public Variables
-	unsigned char I2C_WR_Buf[MAX_BUFFER_SIZE];
-	unsigned char I2C_RD_Buf[MAX_BUFFER_SIZE];
+	unsigned char I2C_WR_Buf[MAX_BUFFER_SIZE];			// Contains data you want to send
+	unsigned char I2C_RD_Buf[MAX_BUFFER_SIZE];			// Contains data which was read
 	
 	// Initialize Functions
-	void i2cOpen();
-	void i2cClose();
-	void i2cSetAddress(int address);
+	void i2cOpen();										// Opens i2cbus 3, done at the beginning
+	void i2cClose();									// Closes i2cbus 3, done at the ending
+	void i2cSetAddress(int address);					// Changes device address
 	
-	// Read/Write Functions
-	void Send_I2C_Byte(unsigned char DEVICE_ADDR, unsigned char Reg_ADDR, unsigned char Data);
+	// Sends a single byte <Data> to <DEVICE_ADDR> on the register <Reg_ADDR>
+	void Send_I2C_Byte(unsigned char DEVICE_ADDR, unsigned char Reg_ADDR, unsigned char Data);	
+
+	// Reads and returns a single byte from <DEVICE_ADDR> on the register <Reg_ADDR>
 	unsigned char Read_I2C_Byte(unsigned char DEVICE_ADDR,unsigned char Reg_ADDR);
+	
+	// Reads multipes byte from <DEVICE_ADDR> starting from the register address <Reg_ADDR>.
+	// Read the output from i2cptr->I2C_RD_Buf
 	unsigned char Read_Multi_Byte(unsigned char DEVICE_ADDR, unsigned char Reg_ADDR);
 };
